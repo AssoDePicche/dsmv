881b1a4f42db616b7bcc3cc63b3fe50734e239cf
AssoDePicche <samuelprado730@gmail.com>
2026-04-03 13:53:10 -0300

    feat: fiber integration

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,12 +1,22 @@
+import './fiber.js';
+
 import { React } from './react.js';
 
 const root = document.getElementById('root');
 
-const element = React.createNode(
-  'div',
-  { style: 'background: salmon; border-radius: 8px; padding: 20px;' },
-  React.createNode('h1', null, 'Mission 1: Success!'),
-  React.createNode('p', null, 'If you can see this, you have eyes')
-);
+const updateApp = (title, description) => {
+  const element = React.createNode(
+    'div',
+    { style: 'background: lightblue; border-radius: 8px; padding: 20px;' },
+    React.createNode('h1', null, title),
+    React.createNode('p', null, description)
+  );
 
-React.render(element, root);
+  React.render(element, root);
+};
+
+const delay = 2000;
+
+updateApp('Mission 3: Fiber Tree Working!', `Wait ${delay / 1000} seconds for the update...`);
+
+setTimeout(updateApp, delay, 'Mission 3: Reconciliation working!', 'The DOM Was Updated');
